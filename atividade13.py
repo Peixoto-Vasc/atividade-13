25103e95e527b17bdc32ace12e87175bde6949c5
Peixoto-Vasc <jpvasconcelospeixoto30@gmail.com>
2024-09-16 09:05:21 -0300

    atv13

---
+++ atividade13.py
@@ -0,0 +1,8 @@
+# Crie um programa que receba um número do usuário e informe se ele é positivo, negativo ou zero.
+numero = int(input())
+if numero>0:
+    print('positivo')
+elif numero<0:
+    print('negativo')
+else:
+    print('zero')
